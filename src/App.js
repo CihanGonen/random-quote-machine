ba70608c74df49270731559d4ba069906deba9fd
Cihan <cihangonen35@gmail.com>
2021-04-28 01:09:48 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,31 @@
-import logo from './logo.svg';
+import {useEffect,useState} from 'react';
 import './App.css';
 
 function App() {
+  const [quote,setQuote] = useState("");
+  useEffect(()=>{
+    fetch('http://api.forismatic.com/api/1.0/?method=getQuote&format=json&lang=en')
+      .then(res => res.json)
+      .then(data=>{
+        console.log(data)}
+       )
+       .catch(err=>console.log(err));
+ 
+  },[])
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="quote-box">
+      <div className="text">
+        
+      </div>
+      <div className="author">
+
+      </div>
+      <div>
+        <button classname="new-quote"></button>
+      </div>
+      <div>
+        <a className="tweet-quote" target='_blank' href="twitter.com/intent/tweet"></a>
+      </div>
     </div>
   );
 }
